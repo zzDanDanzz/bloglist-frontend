fd585508cce3d22a56f33cca17259ef7c93cee6e
zzDanDanzz <94631874+zzDanDanzz@users.noreply.github.com>
2022-01-04 17:57:08 +0330

    5.12

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/jsx-runtime"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react", "jest"
+    ],
+    "rules": {
+    },
+    "settings": {
+        "react": {
+          "version": "detect"
+        }
+      }
+};
