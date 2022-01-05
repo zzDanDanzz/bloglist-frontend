b64e0f90eeadc9fe8fe3f18b6c1fdaa9eb1c8bb4
zzDanDanzz <94631874+zzDanDanzz@users.noreply.github.com>
2022-01-05 18:09:06 +0330

    5.14

--- src/components/Blogs/Blog.test.js
+++ src/components/Blogs/Blog.test.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
+const user = {username: 'dandan', name: 'dan'}
+const blog = { title: "Some title", author: "Johnson", url: "google.com", likes: 23, user}  
+
 test("Displays title and author, but not likes or url", () => {
-  const blog = { title: "Some title", author: "Johnson", url: "google.com", likes: 23}  
 
   const component = render(
-    <Blog blog={blog} />
+    <Blog blog={blog} user={user}/>
   )
 
   expect(component.container).toHaveTextContent(
@@ -17,4 +19,18 @@ test("Displays title and author, but not likes or url", () => {
     /.*google.com.*23/
   )
   
+})
+
+test("'more' button shows url and likes", () => {
+
+  const component = render(
+    <Blog blog={blog} user={user}/>
+  )
+
+  const moreBtn = component.getByText('More')
+
+  fireEvent.click(moreBtn)
+
+  expect(component.container).toHaveTextContent(/Some title.*Johnson.*google.com.*23/)
+
 })
