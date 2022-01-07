2745d5f41062bce4a5c421a49c46a7f5747411f3
zzDanDanzz <94631874+zzDanDanzz@users.noreply.github.com>
2022-01-07 17:05:18 +0330

    5.18

--- cypress/integration/blogapp.spec.js
+++ cypress/integration/blogapp.spec.js
@@ -1,10 +1,48 @@
-describe('Blog app', function() {
-  beforeEach(function() {
+/// <reference types="Cypress" />
+
+const user = {
+  name: 'danny',
+  username: 'boyyy',
+  password: '123123'
+}
+
+describe('Blog app', function () {
+  beforeEach(function () {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    cy.request('POST', 'http://localhost:3003/api/users/', user)
     cy.visit('http://localhost:3000')
   })
 
-  it('Login form is shown', function() {
+  it('Login form is shown', function () {
     cy.get('form').contains('login')
+  })
+
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
+      cy.get('form')
+        .find("input[name='username']")
+        .type(user.username)
+      cy.get('form')
+        .find("input[name='password']")
+        .type(user.password)
+      cy.get('form')
+        .contains('login')
+        .click()
+      cy.contains(`Welcome, ${user.name}`).find('button').should('contain.text', 'logout')
+    })
+
+    it('fails with wrong credentials', function () {
+      cy.get('form')
+        .find("input[name='username']")
+        .type('foo')
+      cy.get('form')
+        .find("input[name='password']")
+        .type('bar')
+      cy.get('form')
+        .contains('login')
+        .click()
+      cy.should('not.contain.text', 'Welcome, ')
+      cy.contains(/invalid (username|password)/).should('have.css', 'color', 'rgb(255, 0, 0)')
+    })
   })
 })
